33232a1baea8680729b4282fdd71ea6a1d531163
kathiravan-spark <kathiravan@sparktech.com>
2022-05-09 18:49:49 +0530

    socket package installation

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,61 @@
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.1.2/socket.io.js"></script>
+<div class="container spark-screen">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('chat') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Chat Module</div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-8" >
+                            <div id="messages" style="border: 1px solid #121212; margin-bottom: 5px; height: 250px; padding: 2px; overflow: scroll;"></div>
+                        </div>
+                        <div class="col-lg-8" >
+                            <form action="sendmessage" method="POST">
+                                @csrf
+                                <input type="hidden" name="user" value="{{ Auth::user()->name }}" >
+                                <textarea class="form-control message"></textarea>
+                                <br/>
+                                <input type="button" value="Send" class="btn btn-success" id="send-message">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    var socket = io.connect('http://localhost:8000');
+    socket.on('message', function (data) {
+        data = jQuery.parseJSON(data);
+        $( "#messages" ).append( "<strong>"+data.user+":</strong><p>"+data.message+"</p>" );
+    });
+    $("#send-message").click(function(e){
+        e.preventDefault();
+        var _token = $("input[name='_token']").val();
+        var user = $("input[name='user']").val();
+        var message = $(".message").val();
+        if(message != ''){
+            $.ajax({
+                type: "POST",
+                url: '{!! URL::to("sendmessage") !!}',
+                dataType: "json",
+                data: {'_token':_token, 'message':message, 'user':user},
+                success:function(data) {
+                    $(".message").val('');
+                }
+            });
+        }
+    })
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/chat', function () {
+    return view('chat');})->name('chat');
+
+Route::post('/sendmessage', [ChatController::class, 'sendMessage'])->name('sendmessage');
